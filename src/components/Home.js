43f195c09cf3cabe047b64ba37e444c17084dfcc
Vivian Gomez <viviangomez0@gmail.com>
2022-04-21 17:12:51 -0400

    added home image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,13 @@
 export const Home = () => {
   return (
     <div>
+      <div className="background">
+        <img src="https://images.unsplash.com/photo-1534232470104-d4188dec5137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGl2ZSUyMGJhcnxlbnwwfHwwfHw%3D&w=1000&q=80"  
+        style={{width: 400, height: 400, borderRadius: 400/ 2}} />
+        <script>
+          document.getElementById('id').style.borderRadius = '50%';
+        </script>
+      </div>
         
     </div>
   )
