29ed714ebf22d18f16ab2042104a4d4fc71ae10d
Vivian Gomez <viviangomez0@gmail.com>
2022-04-21 16:17:56 -0400

    added styling to navbar

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,7 +1,58 @@
+import { NavLink } from "react-router-dom"
+const style ={
+    width: "100%",
+    margin:"5% 0 1%",
+    padding:"1em",
+    textDecoration:"none",
+    color:"green",
+    backgroundColor: "rgb(235, 199, 103)"
+  
 
 
+}
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <NavLink
+
+         activeStyle={{
+              fontWeight:"bold",
+              color:"green"
+         }}
+            exact
+            style={style}
+            to= "/"
+        >Home</NavLink>
+
+<NavLink
+         activeStyle={{
+              fontWeight:"bold",
+              color:"green"
+         }}
+            exact
+            style={style}
+            to= "/Drinks"
+        >Drinks On Tab</NavLink>
+
+<NavLink
+         activeStyle={{
+              fontWeight:"bold",
+              color:"green"
+         }}
+            exact
+            style={style}
+            to= "/Drinks/new"
+        >Add a New Drink</NavLink>
+
+<NavLink
+         activeStyle={{
+              fontWeight:"bold",
+              color:"green"
+         }}
+           exact
+            style={style}
+            to= "/About"
+        >About</NavLink>
+    </div>
   )
 }
