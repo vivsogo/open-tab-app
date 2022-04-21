7295fc504af29ca7b27ee8c50ac14cadcdf27bcd
Vivian Gomez <viviangomez0@gmail.com>
2022-04-21 17:12:25 -0400

    added about <p> tag

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
-
+import React from "react"
 export const About = () => {
   return (
     <div>
-        
+        <h1>Hello and welcome to Open Tab </h1>
+        <p> Open Tab was created as a fun way to save drinks youve had while out for a fun night. 
+          You can simply save your receipe and make your favorite drink at home so you can take the party with you!  </p>
     </div>
   )
 }
