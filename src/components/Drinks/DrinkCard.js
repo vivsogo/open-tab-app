28efe4f4aeca1bd9b6c4118b04c37ab3c7ae2218
Vivian Gomez <viviangomez0@gmail.com>
2022-04-25 16:20:44 -0400

    added drink card components

--- src/components/Drinks/DrinkCard.js
+++ src/components/Drinks/DrinkCard.js
@@ -1,6 +1,10 @@
-
-export const DrinkCard = () => {
+import React from "react"
+export const DrinkCard = ({drink}) => {
   return (
-    <div>DrinkCard</div>
+    <div style={{border:"solid", width:"30%",margin:"auto"}}>
+     <h3>{drink.name}</h3>
+     <img src={drink.image} style={{width: 200, height: 200, borderRadius: 200/ 2}}/>
+     <h4>{drink.recipe}</h4>
+    </div>
   )
 }
