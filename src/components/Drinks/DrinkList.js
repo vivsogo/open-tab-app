65168d34e38a2272ae02fdc16c5ddea35d0915db
Vivian Gomez <viviangomez0@gmail.com>
2022-04-25 16:21:35 -0400

    added map

--- src/components/Drinks/DrinkList.js
+++ src/components/Drinks/DrinkList.js
@@ -1,7 +1,15 @@
+import {DrinkCard} from "./DrinkCard"
+import React from "react"
 
-
-export const DrinkList = () => {
+export const DrinkList = ({drinks}) => {
+  const drinkCards = drinks.map(drink => 
+  <DrinkCard key={drink.id} drink={drink}/>)
   return (
-    <div>DrinkList</div>
-  )
+         <div>
+           <h2>Drinks</h2>
+           <div style={{display:"flex", flexWrap:"wrap"}}>{drinkCards} </div>
+          </div>
+  ) 
 }
+//from object to component 
+// for each drink were going to use map render a drink component  
