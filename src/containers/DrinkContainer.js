1c883275b951dbecd2ba7386167ff751d03efc32
Vivian Gomez <viviangomez0@gmail.com>
2022-04-25 16:21:56 -0400

    fetch data from my drink container

--- src/containers/DrinkContainer.js
+++ src/containers/DrinkContainer.js
@@ -1,5 +1,22 @@
+import { useState,useEffect } from "react"
+import { DrinkCard } from "../components/Drinks/DrinkCard"
+import { DrinkForm } from "../components/Drinks/DrinkForm"
+import {DrinkList} from "../components/Drinks/DrinkList"
 export const DrinkContainer = () => {
+
+  const [drinks,setDrinks] = useState([])
+  const  fetchData = async ()=>{
+    const resp= await fetch("http://localhost:3004/Drinks")
+     const data = await resp.json()
+     setDrinks(data)
+    
+  }
+   useEffect(()=>{
+     fetchData() 
+   },[])
   return (
-    <div></div>
+    <div>
+      <DrinkList drinks={drinks}/> 
+    </div>
   )
 }
