0c814ea8f788b0af56f3a50598f06d96b1118bf2
Vivian Gomez <viviangomez0@gmail.com>
2022-04-25 16:20:56 -0400

    added drink form

--- src/components/Drinks/DrinkForm.js
+++ src/components/Drinks/DrinkForm.js
@@ -1,6 +1,23 @@
-
+import { useState } from "react"
 export const DrinkForm = () => {
+  const[name,setName] =useState(intialState)
+  const[recipe,setRecipe]= useState(intialState)
+  const[image,setImage]= useState(intialState)
+
+  const handleSubmit =e=>{
+    
+  }
   return (
-    <div>DrinkForm</div>
+    <div>
+      <form onSubmit={handleSubmit}> 
+        <label htmlFor= "Drink">Drink </label>
+        <input type="text" name="Drink" id="name"/><br />
+        <label htmlFor= "recipe">Recipe</label>
+        <input type="text" name="recipe" id="recipe"/><br />
+        <label htmlFor= "Image">Image URL</label>
+        <input type="text" name="image" id="image"/><br />
+        <input type="submit" value="create"/>
+      </form>
+    </div>
   )
 }
