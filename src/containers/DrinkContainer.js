47ae78905357bb716b246b282241d71c1c8faaef
Vivian Gomez <viviangomez0@gmail.com>
2022-04-26 15:57:57 -0400

    handlesearch on container

--- src/containers/DrinkContainer.js
+++ src/containers/DrinkContainer.js
@@ -1,22 +1,33 @@
 import { useState,useEffect } from "react"
 import { DrinkCard } from "../components/Drinks/DrinkCard"
+import { DrinkFilter } from "../components/Drinks/DrinkFilter"
 import { DrinkForm } from "../components/Drinks/DrinkForm"
 import {DrinkList} from "../components/Drinks/DrinkList"
 export const DrinkContainer = () => {
 
   const [drinks,setDrinks] = useState([])
+  const[filteredDrinks, setFilteredDrinks] = useState (drinks)
   const  fetchData = async ()=>{
     const resp= await fetch("http://localhost:3004/Drinks")
      const data = await resp.json()
      setDrinks(data)
+     setFilteredDrinks(data)
     
   }
    useEffect(()=>{
      fetchData() 
    },[])
+
+   const handleSearch = (searchValue) => {
+     const filteredDrinks = drinks.filter(drink => drink.name.toLowerCase().startsWith(searchValue.toLowerCase()))
+     console.log(filteredDrinks)
+     setFilteredDrinks(filteredDrinks)
+}
   return (
     <div>
-      <DrinkList drinks={drinks}/> 
+      <DrinkFilter handleSearch={handleSearch}/>
+      <DrinkList drinks={filteredDrinks}/> 
+      
     </div>
   )
 }
