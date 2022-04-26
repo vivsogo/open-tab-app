8be47515d9540197f72150d1a83419ffed491109
Vivian Gomez <viviangomez0@gmail.com>
2022-04-26 13:13:00 -0400

    added fetch for new drink card

--- src/components/Drinks/DrinkForm.js
+++ src/components/Drinks/DrinkForm.js
@@ -1,23 +1,43 @@
 import { useState } from "react"
+import { useHistory } from "react-router-dom"
+
+
 export const DrinkForm = () => {
-  const[name,setName] =useState(intialState)
-  const[recipe,setRecipe]= useState(intialState)
-  const[image,setImage]= useState(intialState)
+  const history = useHistory
+  const[name,setName] =useState("")
+  const[recipe,setRecipe]= useState("")
+  const[image,setImage]= useState("")
 
   const handleSubmit =e=>{
-    
+    e.preventDefault()
+    const newDrink={name,recipe,image}
+
+    fetch("http://localhost:3004/Drinks",{
+      methond:"POST",
+      headers: {
+        "content-Type":"application/json"
+      },
+      body: JSON.stringify(newDrink)
+    })
+    setName("")
+    setRecipe("")
+    setImage("")
+    history.push("/drinks")
+
+
   }
   return (
     <div>
       <form onSubmit={handleSubmit}> 
         <label htmlFor= "Drink">Drink </label>
-        <input type="text" name="Drink" id="name"/><br />
+        <input onChange={e=> setName(e.target.value)} type="text" name="Drink" id="name" vaule={name} required/><br />
         <label htmlFor= "recipe">Recipe</label>
-        <input type="text" name="recipe" id="recipe"/><br />
+        <input onChange={e=> setRecipe(e.target.value)} type="text" name="recipe" id="recipe" vaule={recipe} required/><br />
         <label htmlFor= "Image">Image URL</label>
-        <input type="text" name="image" id="image"/><br />
+        <input onChange={e=> setImage(e.target.value)} type="text" name="image" id="image" vaule={image} required /><br />
         <input type="submit" value="create"/>
       </form>
     </div>
   )
 }
+ 
