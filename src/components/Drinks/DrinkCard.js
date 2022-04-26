a46c340e23d6bbccdd2af47d186dd505140bdf2d
Vivian Gomez <viviangomez0@gmail.com>
2022-04-26 15:56:38 -0400

    added style to border on card

--- src/components/Drinks/DrinkCard.js
+++ src/components/Drinks/DrinkCard.js
@@ -1,7 +1,7 @@
 import React from "react"
 export const DrinkCard = ({drink}) => {
   return (
-    <div style={{border:"solid", width:"30%",margin:"auto"}}>
+    <div style={{ border:"outset",color:"green",background:"rgb(235, 199, 103)",width:"30%",margin:"auto"}}>
      <h3>{drink.name}</h3>
      <img src={drink.image} style={{width: 200, height: 200, borderRadius: 200/ 2}}/>
      <h4>{drink.recipe}</h4>
